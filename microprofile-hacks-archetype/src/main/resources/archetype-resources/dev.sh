926340c40d16727c696dcc461cad325380a0817b
Ghassen Chetioui <ghassen.chetioui@gmail.com>
2019-08-15 21:29:27 +0200

    chore: update dev script to expose console port

--- microprofile-hacks-archetype/src/main/resources/archetype-resources/dev.sh
+++ microprofile-hacks-archetype/src/main/resources/archetype-resources/dev.sh
@@ -3,7 +3,7 @@ mvn clean package
 echo "Building image..."
 docker image build -t ${artifactId} .
 echo "Starting container..."
-docker container run -d --rm --name ${artifactId} -p 8080:8080 -v /tmp/wad-deployments:/opt/jboss/wildfly/standalone/deployments/ ${artifactId}
+docker container run -d --rm --name ${artifactId} -p 8080:8080 -p 9990:9990 -v /tmp/wad-deployments:/opt/jboss/wildfly/standalone/deployments/ ${artifactId}
 echo "Watch and Deploy..."
 java -Dmaven.home=/home/cgh/.sdkman/candidates/maven/3.5.4/ -jar ~/.wad/wad.jar /tmp/wad-deployments
 echo "Stopping container..."
