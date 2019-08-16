f9e06aeb6d94b3480f5ababff02711aaac076163
Ghassen Chetioui <ghassen.chetioui@gmail.com>
2019-08-16 17:55:08 +0200

    chore: update dev scripts to use env vars

--- mp-health-hacks/dev.sh
+++ mp-health-hacks/dev.sh
@@ -1,3 +1,4 @@
+. ../export-env-vars
 echo "Packaging application..."
 mvn clean package
 echo "Building image..."
@@ -5,6 +6,6 @@ docker image build -t mp-health-hacks .
 echo "Starting container..."
 docker container run -d --rm --name mp-health-hacks -p 9080:9080 -v /tmp/wad-deployments:/liberty/usr/servers/defaultServer/dropins/ mp-health-hacks
 echo "Watch and Deploy..."
-java -Dmaven.home=/home/cgh/.sdkman/candidates/maven/3.5.4/ -jar ~/.wad/wad.jar /tmp/wad-deployments
+java -Dmaven.home=${M2_HOME} -jar ${WAD_HOME}/wad.jar /tmp/wad-deployments
 echo "Stopping container..."
 docker container stop mp-health-hacks
