8ecea3130ca6368a3971fdceea6aa1f2d814b433
Ghassen Chetioui <ghassen.chetioui@gmail.com>
2019-08-16 22:09:08 +0200

    chore: expose port 9443

--- microprofile-hacks-archetype/src/main/resources/archetype-resources/dev.sh
+++ microprofile-hacks-archetype/src/main/resources/archetype-resources/dev.sh
@@ -4,7 +4,7 @@ mvn clean package
 echo "Building image..."
 docker image build -t ${artifactId} .
 echo "Starting container..."
-docker container run -d --rm --name ${artifactId} -p 9080:9080 -v /tmp/wad-deployments:/liberty/usr/servers/defaultServer/dropins/ ${artifactId}
+docker container run -d --rm --name ${artifactId} -p 9080:9080 -p 9443:9443 -v /tmp/wad-deployments:/liberty/usr/servers/defaultServer/dropins/ ${artifactId}
 echo "Watch and Deploy..."
 java -Dmaven.home=${M2_HOME} -jar ${WAD_HOME}/wad.jar /tmp/wad-deployments
 echo "Stopping container..."
