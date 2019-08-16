5fda940cd9f331b9a5ac05ae440ca20e48f143a8
Ghassen Chetioui <ghassen.chetioui@gmail.com>
2019-08-16 17:31:56 +0200

    chore: externalize env vars

--- microprofile-hacks-archetype/src/main/resources/archetype-resources/dev.sh
+++ microprofile-hacks-archetype/src/main/resources/archetype-resources/dev.sh
@@ -1,3 +1,4 @@
+. ../export-env-vars
 echo "Packaging application..."
 mvn clean package
 echo "Building image..."
@@ -5,6 +6,6 @@ docker image build -t ${artifactId} .
 echo "Starting container..."
 docker container run -d --rm --name ${artifactId} -p 9080:9080 -v /tmp/wad-deployments:/liberty/usr/servers/defaultServer/dropins/ ${artifactId}
 echo "Watch and Deploy..."
-java -Dmaven.home=/home/cgh/.sdkman/candidates/maven/3.5.4/ -jar ~/.wad/wad.jar /tmp/wad-deployments
+java -Dmaven.home=${M2_HOME} -jar ${WAD_HOME}/wad.jar /tmp/wad-deployments
 echo "Stopping container..."
 docker container stop ${artifactId}
