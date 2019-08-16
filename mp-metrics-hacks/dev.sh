f336abb841f0ff03a9cc899d21a814b5276989a7
Ghassen Chetioui <ghassen.chetioui@gmail.com>
2019-08-16 19:05:35 +0200

    feat: init mp metrics project

---
+++ mp-metrics-hacks/dev.sh
@@ -0,0 +1,11 @@
+. ../export-env-vars
+echo "Packaging application..."
+mvn clean package
+echo "Building image..."
+docker image build -t mp-metrics-hacks .
+echo "Starting container..."
+docker container run -d --rm --name mp-metrics-hacks -p 9080:9080 -v /tmp/wad-deployments:/liberty/usr/servers/defaultServer/dropins/ mp-metrics-hacks
+echo "Watch and Deploy..."
+java -Dmaven.home=${M2_HOME} -jar ${WAD_HOME}/wad.jar /tmp/wad-deployments
+echo "Stopping container..."
+docker container stop mp-metrics-hacks
